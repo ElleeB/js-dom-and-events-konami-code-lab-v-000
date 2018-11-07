c8958a78fc8924c95d2897c3755bec88406c844a
ElleeB <laura.trager@gmail.com>
2018-11-07 17:37:03 -0500

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,12 +1,11 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-let index = 0
-
 function init() {
-  const doc = document
 
-  doc.addEventListener('keydown', function(event) {
-    const key = parseInt(event.which)
+  let index = 0
+
+  document.body.addEventListener('keydown', function(event) {
+    const key = parseInt(event.which || event.detail)
 
     if (key === code[index]) {
       index++
